c2b0482e48c1fecd2de9b4e9542b6c5d1fac3101
Ola <129540485+Maladelfineka@users.noreply.github.com>
2025-01-22 18:29:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert ("hi");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const colorPicker = document.getElementById("color");
+const sizePicker = document.getElementById("size");
+const clearButton = document.getElementById("clear");
